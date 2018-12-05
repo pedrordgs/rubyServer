3905c71c202a72bf15a03ad63657629079bf7282
pedrordgs <pedrombr99@gmail.com>
2018-12-05 12:02:01 +0000

    accept multiple clients

--- rb/request.rb
+++ rb/request.rb
@@ -1,7 +1,7 @@
-  def parse(request)
-   method, path = request.lines[0].split
-   {
-     path: path,
-     method: method,
-   }
-  end
+def parse(request)
+  method, path = request.lines[0].split
+  {
+  path: path,
+  method: method,
+  }
+end
--- server.rb
+++ server.rb
@@ -5,12 +5,13 @@ require_relative 'rb/answer'
 server = TCPServer.new("localhost",8080)
 
 loop{
-  client=server.accept
-  request=client.gets
-  request=parse(request)
-  answer=prepare(request)
-  puts "#{client.peeraddr[3]} #{request.fetch(:path)} - #{answer.code}"
-  answer.send(client)
-  client.close
+  Thread.start(server.accept) do |client|
+   request=client.gets
+   request=parse(request)
+   answer=prepare(request)
+   puts "#{client.peeraddr[3]} #{request.fetch(:path)} - #{answer.code}"
+   answer.send(client)
+   client.close
+   end
 }
 
